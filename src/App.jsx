bf28119fb337c0ab438f4393fa0c11ec4bc2409b
5urafel <surafel5540f@gmail.com>
2024-12-20 19:23:36 +0000

    Navbar
    the navbar and the background voice added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <main>
+      <Navbar />
       <Hero />
       <About />
     </main>
